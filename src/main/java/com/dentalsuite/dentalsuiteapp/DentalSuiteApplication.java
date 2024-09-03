cd8488bc76037f62de9b1f808343f4bfcee1e87e
nigelnkundhlande <nigelnkundhlande@gmail.com>
2024-09-03 15:55:18 +0200

    Edited Readme.md file

---
+++ src/main/java/com/dentalsuite/dentalsuiteapp/DentalSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.dentalsuite.dentalsuiteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalsuiteappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalsuiteappApplication.class, args);
+    }
+
+}
